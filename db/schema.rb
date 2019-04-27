725eddf28a668c74751debb8612835a4b4851cd4
hasegawa hirofumi <hase1969.hiro@gmail.com>
2019-04-27 22:45:54 +0900

    databaseにvalidationを追記

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_04_26_075507) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
