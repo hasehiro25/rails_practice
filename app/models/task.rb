1703b28fbf38adbe90456f89cce4545214ee4a93
hasegawa hirofumi <hase1969.hiro@gmail.com>
2019-04-29 17:14:45 +0900

    ,(カンマ）が名称に入った場合保存しないよう修正（独自メソッドの練習）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
+  validate :valdiate_name_not_including_comma
+
+  private
+
+  def valdiate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることができません') if  name&.include?(',')
+  end
 end
