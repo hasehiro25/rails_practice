827614205567ec2ac1624613c7d8822b64b603d6
hasegawa hirofumi <hase1969.hiro@gmail.com>
2019-04-30 00:27:20 +0900

    メールアドレスとパスワードが合致した場合cookieを作るよう実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   namespace :admin do
     resources :users
   end
